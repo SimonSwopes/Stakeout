bb31f12ccfdf45527d25a84c9a512e6638cb96fb
SimonSwopes <simon.r.swopes@gmail.com>
2025-01-13 20:06:11 -0600

    refactored for later extension

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
+import sys
 import pandas as pd
+from numpy import random
+from glob import glob
+from os import path
 
 def main():
-    # Small Script to see a preview of the labeled data
-    filePath = 'Data\Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv'
-    data = pd.read_csv(filePath)
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <directory_path>")
+        sys.exit(1)
 
+    directory_path = sys.argv[1]
+    data = load_data(directory_path)
     print(data.head())
-    print(data.columns)
+    print(len(data))
+
+
+def load_data(directory_path: str) -> pd.DataFrame:
+    csv_files = glob(path.join(directory_path, '*.csv'))
+
+    if not csv_files:
+        print("No CSV files found in the specified directory")
+        sys.exit(1)
+
+    dataSets = []
+
+    for file in csv_files:
+            dataSets.append(pd.read_csv(file))
+    
+    return format_data(pd.concat(dataSets, ignore_index=True))
+
+def format_data(data: pd.DataFrame) -> pd.DataFrame:
+    data['Source IP'] = synthesize_ip(len(data))
+    data['Destination IP'] = synthesize_ip(len(data))
+
+    return data[['Source IP', 'Destination IP', 'Flow Duration', 'Total Fwd Packets', 
+           'Total Backward Packets', 'Label']]
+
+
+def synthesize_ip(num : int) -> list:
+    return [f"192.168.{random.randint(0, 256)}.{random.randint(0, 256)}" for _ in range(num)]
 
 if __name__ == '__main__':
     main()
